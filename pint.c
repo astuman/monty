6ff45fd35184acd6a64839bea7edcfe04787343b
Astewul <loloalex1992@gmail.com>
2022-07-15 11:33:14 +0300

    commiting

--- pint.c
+++ pint.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
  * pint - prints the value at the top of the stack
  * @stack: stack structure
@@ -8,8 +7,6 @@
 
 void pint(stack_t **stack, unsigned int line_number)
 {
-
-
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
