211c55bb5fad41b4767881f9cc250c22c26d32c2
Astewul <loloalex1992@gmail.com>
2022-07-15 11:20:23 +0300

    comm

--- monty.h
+++ monty.h
@@ -14,47 +14,47 @@
 /* Structs and Lists */
 
 /**
- *  * struct stack_s - doubly linked list representation of a stack (or queue)
- *   * @n: integer
- *    * @prev: points to the previous element of the stack (or queue)
- *     * @next: points to the next element of the stack (or queue)
- *      *
- *       * Description: doubly linked list node structure
- *        * for stack, queues, LIFO, FIFO Holberton project
- *         */
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct stack_s
 {
-		int n;
-			struct stack_s *prev;
-				struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
- *  * struct instruction_s - opcode and its function
- *   * @opcode: the opcode
- *    * @f: function to handle the opcode
- *     *
- *      * Description: opcode and its function
- *       * for stack, queues, LIFO, FIFO Holberton project
- *        */
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct instruction_s
 {
-		char *opcode;
-			void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /**
- *  * struct list_s - linked list of struct instruction
- *   * @n: number of line
- *    * @inst: instruction of opcode and its function
- *     * @next: points to the next element of the linked list
- *      * Description: Single linked list of the struct instruction
- *       */
+ * struct list_s - linked list of struct instruction
+ * @n: number of line
+ * @inst: instruction of opcode and its function
+ * @next: points to the next element of the linked list
+ * Description: Single linked list of the struct instruction
+ */
 typedef struct list_s
 {
-		int n;
-			char *inst;
-				struct list_s *next;
+	int n;
+	char *inst;
+	struct list_s *next;
 } list_t;
 
 extern list_t *list_opcode;
@@ -100,4 +100,3 @@ void rotl(stack_t **stack, UN unsigned int line_number);
 void rotr(stack_t **stack, UN unsigned int line_number);
 
 #endif /* MONTY_H_ */
-
